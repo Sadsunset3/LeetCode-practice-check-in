69bbe8f800cb6ff853b45bd4617fb34732b8afd8
zhangsan <zhangsan@PKU.com>
2025-12-28 19:06:39 +0800

    2025.12.28

--- Array/src/test/java/com/SadSunset/Hashtable.java
+++ Array/src/test/java/com/SadSunset/Hashtable.java
@@ -149,4 +149,126 @@ public class Hashtable {
         return res;
     }
 
+
+    //383.赎金信
+    //给定一个赎金信 (ransom) 字符串和一个杂志(magazine)字符串，
+    // 判断第一个字符串 ransom 能不能由第二个字符串 magazines 里面的字符构成。如果可以构成，返回 true ；否则返回 false。
+    //(题目说明：为了不暴露赎金信字迹，要从杂志上搜索各个需要的字母，组成单词来表达意思。杂志字符串中的每个字符只能在赎金信字符串中使用一次。)
+    //ransomNote 和 magazine 由小写英文字母组成
+    @Test
+    public  void Tile383() {
+        String ransomNote = "a", magazine = "b";
+        System.out.println(Answer383(ransomNote,magazine));
+    }
+    public boolean Answer383(String ransomNote, String magazine) {
+        int[] recod=new int[26];
+        for (int i = 0; i < ransomNote.length(); i++) {
+            recod[ransomNote.charAt(i)-'a']+=1;
+        }
+        for (int i = 0; i <magazine.length() ; i++) {
+            recod[magazine.charAt(i)-'a']-=1;
+        }
+        for (int a:recod){
+            if(a>0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    //15. 三数之和
+    //给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？请你找出所有满足条件且不重复的三元组。
+    //注意： 答案中不可以包含重复的三元组。
+    //示例：
+    //给定数组 nums = [-1, 0, 1, 2, -1, -4]，
+    //满足要求的三元组集合为： [ [-1, 0, 1], [-1, -1, 2] ]
+
+    @Test
+    public  void Tile15() {
+        int[] nums = {-1,0,1,2,-1,-4};
+        List<List<Integer>> list=Answer15(nums);
+        System.out.println(Answer15(nums));
+    }
+    public List<List<Integer>> Answer15(int[] nums) {
+        //双指针解法
+        //定义结果集
+        List<List<Integer>> result=new ArrayList<>();
+        //定义左右指针
+        //对数组排序
+        Arrays.sort(nums);
+        //遍历数组
+        for (int i = 0; i < nums.length; i++) {
+            if(nums[i]>0){
+                return result;
+            }
+            int left=i+1,right=nums.length-1;
+            if (i > 0 && nums[i] == nums[i - 1]) {  // 去重a
+                continue;
+            }
+            while (left<right){
+                int sum=nums[i]+nums[left]+nums[right];
+                if(sum>0){
+                    right--;
+                } else if (sum<0) {
+                    left++;
+                } else{
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    while (left<right && nums[left]==nums[left+1])left++;
+                    while (left<right && nums[right]==nums[right-1])right--;
+
+                        left++;
+                        right--;
+                }
+            }
+        }
+        return result;
+    }
+
+
+
+
+    //18. 四数之和
+    //题意：给定一个包含 n 个整数的数组 nums 和一个目标值 target，判断 nums 中是否存在四个元素 a，b，c 和 d ，
+    // 使得 a + b + c + d 的值与 target 相等？找出所有满足条件且不重复的四元组。
+    //注意：
+    //答案中不可以包含重复的四元组。
+    //示例： 给定数组 nums = [1, 0, -1, 0, -2, 2]，和 target = 0。 满足要求的四元组集合为：
+    // [ [-1, 0, 0, 1], [-2, -1, 1, 2], [-2, 0, 0, 2] ]
+    @Test
+    public  void Tile18() {
+        int[] nums = {1,0,-1,0,-2,2};
+        int target=0;
+        List<List<Integer>> list=Answer15(nums);
+        System.out.println(Answer15(nums));
+    }
+
+    public List<List<Integer>> Answer18(int[] nums, int target) {
+        List<List<Integer>> result=new ArrayList<>();
+        Arrays.sort(nums);
+        for (int k = 0; k < nums.length; k++) {
+            // 剪枝处理
+            if (nums[k] > target && nums[k] >= 0)break;
+            // 对nums[k]去重
+            if (k > 0 && nums[k] == nums[k - 1])continue;
+            for (int i = k+1; i <nums.length ; i++) {
+                if (i > k + 1 && nums[i] == nums[i - 1]) continue;
+                int right=nums.length-1,left=i + 1;
+                while (left<right) {
+                    int sum = nums[i] + nums[k] + nums[right] + nums[left];
+                    if (sum > target) right--;
+                    else if (sum < target) left++;
+                    else {
+                        result.add(Arrays.asList(nums[i], nums[k], nums[right], nums[left]));
+                            while (left < right && nums[left] == nums[left + 1]) left++;
+                            while (left < right && nums[right] == nums[right - 1]) right--;
+                            left++;
+                            right--;
+
+                    }
+                }
+            }
+        }
+            return result;
+    }
+
 }
